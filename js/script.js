c13f627a03b43935af07bef610c941ca110fd344
Nickster-fed <71288897+Nickster-fed@users.noreply.github.com>
2021-01-24 21:44:29 +0100

    Up to question 4

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+
+
+// Question 1:
+
+let cat = {
+    complain: function() {
+        console.log("Meow!");
+    },
+};
+
+
+
+// Question 2:
+
+const heading= document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+
+// Question 3:
+
+heading.style.fontSize = "2em";
+
+
+
+// Question 4:
+
+heading.className = "subheading;"
+
+
+/*
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+*/
